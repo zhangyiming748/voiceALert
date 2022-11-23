ff70552cd42d5e42eab6d95ef60102f4698c3edb
zen <zhangyiming748@gmail.com>
2022-11-24 01:41:28 +0800

    固定发音人

--- voiceAlert.go
+++ voiceAlert.go
@@ -1,10 +1,8 @@
 package voiceAlert
 
 import (
-	"log"
 	"os/exec"
 	"runtime"
-	"strings"
 )
 
 const (
@@ -19,9 +17,7 @@ func Voice(msg int) {
 	case "darwin":
 		// 查询发音人 `say -v ?`
 		voice := "Serena"
-		if !HasSpoker(voice) {
-			voice = "Victoria"
-		}
+
 		switch msg {
 		case success:
 			cmd = exec.Command("say", "-v", voice, "Rocket was launched successfully")
@@ -51,34 +47,4 @@ func Voice(msg int) {
 		}
 	}
 	cmd.Wait()
-}
-
-func HasSpoker(key string) bool {
-	cmd := exec.Command("say", "-v", "?")
-	stdout, err := cmd.StdoutPipe()
-	cmd.Stderr = cmd.Stdout
-	if err != nil {
-		log.Printf("cmd.StdoutPipe产生的错误:%v\n", err)
-	}
-	if err = cmd.Start(); err != nil {
-		log.Printf("cmd.Run产生的错误:%v\n", err)
-	}
-	for {
-		tmp := make([]byte, 1024)
-		_, err := stdout.Read(tmp)
-		//写成输出日志
-		t := string(tmp)
-		t = strings.Replace(t, "\u0000", "", -1)
-		if strings.Contains(t, key) {
-			return true
-		}
-		//log.Println(t)
-		if err != nil {
-			break
-		}
-	}
-	if err = cmd.Wait(); err != nil {
-		log.Println("命令执行中有错误产生", err)
-	}
-	return false
 }
