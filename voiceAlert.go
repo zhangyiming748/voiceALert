6fa17a4f67083c51a63302ae17b4cc9524026b38
zen <zhangyiming748@gmail.com>
2023-02-09 12:48:16 +0800

    添加了筛选后的mac端讲述人 使用单独的函数分别为两个平台提供发音服务 向前兼容的旧函数会发出outofdate警告

--- voiceAlert.go
+++ voiceAlert.go
@@ -11,46 +11,78 @@ const (
 	FAILED              // 转码失败,程序退出
 	COMPLETE            // 转码进程完成
 )
+const (
+	Default  = Serena
+	Allison  = "Allison"  // 深沉美式女声
+	Ava      = "Ava"      // 深沉美式女声
+	Daniel   = "Daniel"   // 正式英式男声
+	Lanlan   = "Lanlan"   // 童声中文女声
+	Meijia   = "Meijia"   // 正式中文女声
+	Lilian   = "Lilian"   // 柔和中文女声
+	Samantha = "Samantha" // 正经美式女声
+	Serena   = "Serena"   // 沉稳英式女声
+	Shanshan = "Shanshan" // 浑厚中文女声
+	Shasha   = "Shasha"   // 成熟中文女声
+	Sinji    = "Sinji"    // 粤语中文女声
+	Tingting = "Tingting" // 机械中文女声
+	Victoria = "Victoria" // 性感美式女声
+)
 
+var (
+	cmd *exec.Cmd
+)
+
+/*
+运行在mac上的发声命令
+*/
+func CustomizedOnMac(spoker, content string) {
+	defer func() {
+		if err := recover(); err != nil {
+			log.Warn.Printf("执行发生命令出现错误:%v", err)
+		}
+	}()
+	cmd = exec.Command("say", "-v", spoker, content)
+	cmd.Run()
+}
+
+/*
+运行在linux上的发声命令
+*/
+func CustomizedOnLinux(content string) {
+	defer func() {
+		if err := recover(); err != nil {
+			log.Warn.Printf("执行发生命令出现错误:%v", err)
+		}
+	}()
+	//espeak "Testing espeak from the Ubuntu 18.04 terminal"
+	cmd = exec.Command("espeak", content)
+	cmd.Run()
+}
 func Voice(msg int) {
 	defer func() {
+		log.Warn.Printf("这是一个即将废弃的函数\n请及时切换到CustomizedOnMac或CustomizedOnLinux函数\n")
 		if err := recover(); err != nil {
 			log.Debug.Printf("执行发生命令出现错误:%v", err)
 		}
 	}()
-	var cmd *exec.Cmd
 	switch runtime.GOOS {
 	case "darwin":
-		// 查询发音人 `say -v ?`
-		voice := "Serena"
-
 		switch msg {
 		case SUCCESS:
-			cmd = exec.Command("say", "-v", voice, "Rocket was launched successfully")
-			cmd.Start()
+			CustomizedOnMac(Victoria, "Rocket was launched successfully")
 		case FAILED:
-			cmd = exec.Command("say", "-v", voice, "Rocket launch FAILED")
-			cmd.Start()
+			CustomizedOnMac(Victoria, "Rocket launch FAILED")
 		case COMPLETE:
-			cmd = exec.Command("say", "-v", voice, "mission COMPLETE!")
-			cmd.Start()
+			CustomizedOnMac(Victoria, "Mission COMPLETE!")
 		}
 	case "linux":
-		cmd = exec.Command("echo", "-e", "\\a")
 		switch msg {
 		case SUCCESS:
-			for i := 0; i < 2; i++ {
-				cmd.Start()
-			}
+			CustomizedOnLinux("Rocket was launched successfully")
 		case FAILED:
-			for i := 0; i < 50; i++ {
-				cmd.Start()
-			}
+			CustomizedOnLinux("Rocket launch FAILED")
 		case COMPLETE:
-			for i := 0; i < 100; i++ {
-				cmd.Start()
-			}
+			CustomizedOnLinux("Rocket launch FAILED")
 		}
 	}
-	cmd.Wait()
 }
