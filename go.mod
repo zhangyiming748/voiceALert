7bd018176be6658dc53bdb92cb9a2feba449f0b8
zen <zhangyiming748@gmail.com>
2023-02-09 12:51:15 +0800

    函数通过本地测试

--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module github.com/zhangyiming748/voiceAlert
 
 go 1.19
 
-require github.com/zhangyiming748/log v0.0.3
+require github.com/zhangyiming748/log v0.0.4
--- main_test.go
+++ main_test.go
@@ -15,3 +15,12 @@ func BenchmarkVoiceAlert(b *testing.B) {
 		b.Log(b.N)
 	}
 }
+func TestCustomizedOnMacWithChinese(t *testing.T) {
+	words := "我有一对大山雀和一只小猫咪"
+	CustomizedOnMac(Shasha, words)
+}
+
+func TestCustomizedOnMacWithEnglish(t *testing.T) {
+	words := "i have a pair of big tits and a small pussy"
+	CustomizedOnMac(Victoria, words)
+}
